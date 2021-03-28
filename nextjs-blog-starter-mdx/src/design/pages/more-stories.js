c1ecf24af7442ba243fb77d437e955d4633c6218
ANDRIC LIBRESINN <Andric.Libresinn@libertymutual.com>
2021-03-28 00:57:10 -0400

    Getting sidebar mock working

--- nextjs-blog-starter-mdx/src/design/pages/more-stories.js
+++ nextjs-blog-starter-mdx/src/design/pages/more-stories.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Sidebar from '../components/sidebar'
 
 // import PostPreview from '../components/post-preview'
 // const isEmpty = obj => Object.keys(obj).length === 0
@@ -9,11 +10,16 @@ export default function MoreStories({ posts }) {
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div>
+      
+      <div className="left-side">
+        <Sidebar />
+      </div>
+      
+      <div className="body-side">
         {posts.map((post) => (
           <h3 key={post.meta.slug}>
             <Link as={`/posts/${post.meta.slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{post.meta.title}</a>
+              <a className="hover:underline">→ {post.meta.title}</a>
             </Link>
           </h3>
         ))}
