dd7ec2952c475524ef1dbbcc74c52603fa4203ed
Andric LibreSinn <andric.droid@gmail.com>
2021-03-28 16:38:56 -0400

    Feature/simplify (#3)
    * Moving things around
    
    * Moving things around

--- react-static-test/src/components/nav/next-prev.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default () => (
-  <div className="footer-btn d-flex justify-content-between">
-    <a href="#" className="btn"><i className="fas fa-arrow-circle-left"></i>Previous</a>
-    <a href="#" className="btn">Next<i className="fas fa-arrow-circle-right"></i></a>
-  </div>
-)
