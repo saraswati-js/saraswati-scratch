fb9e6085a4c2d6789e623b7ece98ae03e3b069ad
Andric LibreSinn <andric.droid@gmail.com>
2021-04-01 22:10:23 -0400

    Adding the math plugin

--- src/design/layouts/docs/index.js
+++ src/design/layouts/docs/index.js
@@ -5,6 +5,7 @@ import Sidebar from '@components/sidebar'
 export default function DocumentationPage({ children }) {
   return (
     <div>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossOrigin="anonymous"></link>
       <NavPrimary />      
       <div className="d-flex" id="wrapper">
         <div className="bg-light border-right" id="sidebar-wrapper">
